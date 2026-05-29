f138a6858c0e5a813c63aaad2042921db6cbf585
Anjali <ajanjali07@gmail.com>
2026-05-29 14:24:56 +0530

    Create redis.js

---
+++ server/utils/redis.js
@@ -0,0 +1,14 @@
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL || 'redis://127.0.0.1:6379'
+});
+
+redisClient.on('error', (err) => console.error('Redis Client Error', err));
+
+await redisClient.connect().then(() => console.log('Redis Connected Successfully'));
+
+export default redisClient;
